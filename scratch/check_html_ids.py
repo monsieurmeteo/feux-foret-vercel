dc3fec1704e3ea0591aee44dd7037b6eecbaf1e3
Gregory Langlet <langlet.gregory@gmail.com>
2026-07-23 15:24:44 +0200

    clean: remove temporary test files from scratch directory

--- scratch/check_html_ids.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open("index.html", "r", encoding="utf-8") as f:
-    content = f.read()
-
-# Let's search for hist-container- inside the file
-matches = re.findall(r'id=["\']hist-container-([^"\']+)["\']', content)
-print("hist-container- IDs found in index.html:", matches[:10])
-print("Total matches:", len(matches))
